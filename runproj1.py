f702b18519479a8d95affcd0ab8d0199decfc146
Adam Sunderman <sunderad@oregonstate.edu>
2018-04-20 14:21:19 -0700

    results added/code done

--- runproj1.py
+++ runproj1.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python
+import os
 
-cmd = "g++ -DNUMT=1 -DNUMNODES=512 proj1.cpp lib_cs475.cpp -o proj1 -lm -fopenmp"
+for thread in [1,2,4,8]:
+	for node in [64,128,256,512,1024,2048,4096,8192]:
+		ccmd = "g++ -DNUMT=%d -DNUMNODES=%d proj1.cpp lib_cs475.cpp -o proj1 -lm -fopenmp" %(thread, node);
+		rcmd = "./proj1";
+		os.system(ccmd);
+		os.system(rcmd);
